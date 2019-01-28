fe597a46ba776f1d53480d2c18dd0aed66e2535f
Raul Ramirez <raul.ramirez@escuelaestech.es>
2019-01-28 14:07:18 +0100

    gridlayout_cards

---
+++ app/src/main/java/com/example/alumno_fp/table_grid_layout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.alumno_fp.table_grid_layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.gridlayout_cards);
+    }
+}
