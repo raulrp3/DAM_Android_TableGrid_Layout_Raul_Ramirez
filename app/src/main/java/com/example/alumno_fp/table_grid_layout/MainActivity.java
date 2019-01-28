9177da26226f3750e71f8fc98ff180c42ee5c171
Raul Ramirez <raul.ramirez@escuelaestech.es>
2019-01-28 14:48:21 +0100

    tablelayout_calendar

--- app/src/main/java/com/example/alumno_fp/table_grid_layout/MainActivity.java
+++ app/src/main/java/com/example/alumno_fp/table_grid_layout/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        setContentView(R.layout.gridlayout_cards);
+        //setContentView(R.layout.gridlayout_cards);
+        setContentView(R.layout.tablelayout_calendar);
     }
 }
